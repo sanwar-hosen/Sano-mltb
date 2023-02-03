4d6e9924f58a0ce743a1ad2de06c40f66bcdd054
Sano <amishuvo1230@gmail.com>
2023-02-03 18:05:19 +0600

    merge to latest JMDKH commit

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -275,8 +275,12 @@ def is_gdrive_link(url: str):
     return "drive.google.com" in urlparse(url).netloc
 
 def is_Sharerlink(url: str):
-    return bool(match(r'https?:\/\/.+\.gdtot\.\S+|https?:\/\/(filepress|filebee|appdrive|gdflix)\.\S+', url))
-
+    if 'gdtot' in url:
+        regex = r'(https?:\/\/.+\.gdtot\..+\/file\/\d+)'
+    else:
+        regex = r'(https?:\/\/(\S+)\..+\/file\/\S+)'
+    return bool(match(regex, url))
+    
 def is_mega_link(url: str):
     url_ = urlparse(url)
     return any(x in url_.netloc for x in ['mega.nz', 'mega.co.nz'])
